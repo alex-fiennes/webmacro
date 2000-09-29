523d570654065be8ed5da5108c4e71258f5f90b7
brian <brian>
2000-09-29 03:14:53 +0000

    Add comments

--- webmacro/src/org/webmacro/directive/DirectiveDescriptor.java
+++ webmacro/src/org/webmacro/directive/DirectiveDescriptor.java
@@ -3,11 +3,32 @@ package org.webmacro.directive;
 import org.webmacro.directive.Directive.ArgDescriptor;
 import org.webmacro.directive.Directive.Subdirective;
 
+/**
+ * Each directive needs a DirectiveDescriptor to describe how it
+ * should be parsed and built by the parser.  The directive descriptor
+ * identifies the directive's name, the class of the underlying concrete
+ * directive object, a list of directive argument descriptors, and a list
+ * of subdirective descriptors.  
+ *
+ * The args field is an array of Directive.ArgDescriptor objects.  There
+ * are static nested classes within Directive for each type of argument 
+ * -- Condition, LValue, RValue, Keyword, Punctuation, Block, LiteralBlock, 
+ * and special argument descriptors for OptionalGroup and 
+ * OptionalRepeatingGroup.  These allow the directive writer to specify 
+ * a flexible syntax for directive arguments.  
+ * Each directive can have a set of subdirectives, and each subdirective
+ * can have its own argument list.  Subdirectives can be required, optional, 
+ * or optional-repeating (multiple subdirectives of the same kind, like 
+ * #elseif.)  There are constructors for Subdirective in the Directive module
+ * as well.  
+ */
+
 public final class DirectiveDescriptor { 
   public String                   name;
   public Class                    dirClass;
   public ArgDescriptor[]          args;
   public Subdirective[]           subdirectives;
+
   public boolean                  valid=false;
 
   public DirectiveDescriptor(String name, 
