c96915224e2829263673a331f810da4094f0be5b
brian <brian>
2000-10-17 01:01:35 +0000

    Make BuildException support RethrowableException constructors

--- webmacro/src/org/webmacro/engine/BuildException.java
+++ webmacro/src/org/webmacro/engine/BuildException.java
@@ -8,4 +8,10 @@ public class BuildException extends TemplateException
    public BuildException(String message) {
       super(message);
    }
+
+   public BuildException(String reason, Exception e) 
+   {
+      super(reason, e);
+   }
+
 }
