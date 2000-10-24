ccb81a04ece4cb7438f0f032f16a93894c780d1e
justin <justin>
2000-10-24 07:02:19 +0000

    -- A map with sensible internal synchronization. In particular more      than one thread can access it simultaneously without blocking,      provided that they access different buckets within the map.

---
+++ webmacro/src/org/webmacro/util/SimpleMap.java
@@ -0,0 +1,191 @@
+
+package org.webmacro.util;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.LinkedList;
+
+class MapNode {
+   final Object key;
+   Object value;
+   MapNode next;
+
+   MapNode(Object key) {
+      this.key = key;
+   }
+}
+
+
+/**
+  * This map has a fixed number of buckets. Each bucket is an LRU 
+  * cache. The map never increases the number of buckets once it has
+  * been created. The advantage is that it synchronizes on the bucket
+  * so multiple threads can access the map simultaneously without 
+  * blocking, providing they access different buckets.
+  */
+final public class SimpleMap 
+{
+   private MapNode[] _map;
+   private Object[] _locks;
+
+   /**
+     * Create a new SimpleMap with 1001 LRU buckets
+     */
+   public SimpleMap() {
+      this(1001);
+   }
+
+   /**
+     * Create a new SimpleMap with 'size' LRU buckets
+     */
+   public SimpleMap(int size) {
+      _map = new MapNode[size];      
+      _locks = new Object[size];
+      for (int i = 0; i < size; i++) {
+         _locks[i] = new Object();
+      }
+   }
+
+   /**
+     * Add a key to the SimpleMap. 
+     */
+   public void put(Object key, Object value) {
+      if (key == null) {
+         return;
+      }
+      if (value == null) {
+         remove(key);
+         return;
+      }
+      int hash = key.hashCode() % _map.length;
+      synchronized(_locks[hash]) {
+         MapNode m = _map[hash];
+         while (m != null) {
+            if ((m.key == key) || (m.key.equals(key))) {
+               m.value = value;
+               return;
+            }
+            m = m.next;
+         }
+         m = new MapNode(key);
+         m.value = value;
+         m.next = _map[hash];
+         _map[hash] = m;
+      }
+   }
+
+   /**
+     * Get the value of 'key' back. Returns null if no such key.
+     */
+   public Object get(Object key) {
+      int hash = key.hashCode() % _map.length;
+      MapNode last = null;
+      synchronized(_locks[hash]) {
+         MapNode m = _map[hash];
+         if (m == null) return null;
+         while (m != null) {
+            if ((m.key == key) || (m.key.equals(key))) {
+               if (last != null) {
+                  last.next = m.next;
+                  m.next = _map[hash];
+                  _map[hash] = m;
+               }
+               return m.value;
+            }
+            last = m;
+            m = m.next;
+         } 
+      }
+      return null;
+   }
+
+   /**
+     * Ensure that the key does not appear in the map
+     */
+   public void remove(Object key) {
+      int hash = key.hashCode() % _map.length;
+      synchronized(_locks[hash]) {
+         MapNode m = _map[hash];
+         if (m == null) return;
+         MapNode last = null;
+         while (m != null) {
+            if ((m.key == key) || (m.key.equals(key))) {
+               if (last == null) {
+                  _map[hash] = m.next; // may be to null :-)
+               } else {
+                  last.next = m.next;
+               }
+               return;
+            }
+            last = m;
+            m = m.next;
+         }
+      }
+   }
+
+   /**
+     * Returns an iterator that will walk along a snapshot of the keys of 
+     * this SimpleMap. If the Map changes during the creation of this 
+     * iterator some values may be missed or included twice, but otherwise
+     * it will work. The remove() method on this iterator is well defined.
+     */
+   public Iterator iterator() {
+      LinkedList ll = new LinkedList();
+      for (int i = 0; i < _map.length; i++) {
+         synchronized(_locks[i]) {
+            MapNode m = _map[i];
+            while (m != null) {
+               ll.add(m.key);
+               m = m.next;
+            }
+         }
+      }
+      final Iterator lli = ll.iterator();
+      return new Iterator() {
+         Object last = null;
+
+         public boolean hasNext() {
+            return lli.hasNext();
+         }
+
+         public Object next() {
+            last = lli.next();
+            return last;
+         }
+
+         public void remove() {
+            SimpleMap.this.remove(last);
+         }
+      };
+   }
+
+   public static void main(String arg[]) {
+
+      SimpleMap sm = new SimpleMap(3);
+
+      for (int i = 0; i < arg.length; i++) {
+         Integer oi = new Integer(i);
+         System.out.println("*** Adding " + arg[i] + " = " + oi);
+         sm.put(arg[i], oi);
+         for (int j = 0; j < arg.length; j++) {
+            System.out.println(arg[j] + " ==> " + sm.get(arg[j]));      
+         }
+         System.out.println("==========\n");
+      }
+
+      System.out.println("\nACCESSORS:\n");
+      Iterator it1 = sm.iterator();
+      while (it1.hasNext()) {
+         String key = (String) it1.next();
+         System.out.println("*** Removing " + key);
+         it1.remove();
+
+         Iterator it2 = sm.iterator();
+         while (it2.hasNext()) {
+            String key2 = (String) it2.next();
+            System.out.println(key2 + " => " + sm.get(key2));
+         }
+         System.out.println("==========\n");
+     }
+   }
+}
