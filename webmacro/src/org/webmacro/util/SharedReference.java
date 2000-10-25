a46431679f361d457b0e5b3afe88881514c51922
justin <justin>
2000-10-25 09:50:17 +0000

    -- checking in utility class

---
+++ webmacro/src/org/webmacro/util/SharedReference.java
@@ -0,0 +1,120 @@
+
+package org.webmacro.util;
+
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+
+/**
+  * Share an object with minimal synchronization
+  */
+final public class SharedReference implements Cloneable 
+{
+   final private class SharedNode { Reference r; }
+   private final SharedNode shared = new SharedNode();
+   private Reference local;
+
+   /**
+     * Create a new SoftReference to o and wrap it in a SharedReference
+     */
+   public SharedReference(Object o) {
+      shared.r = local = new SoftReference(o);
+   }
+
+   public boolean equals(Object o) {
+      return ((this == o) || 
+              ((o instanceof SharedReference) 
+                && ((SharedReference) o).shared == shared));
+   }
+
+   public int hashCode() {
+      Object o = get();
+      return (o != null) ? o.hashCode() : 0;
+   }
+
+   public String toString() {
+      Object o = get();
+      return (o != null) ? o.toString() : null;
+   }
+
+   /**
+     * This method must be called by only a single thread.
+     * Get a copy of the Object.
+     */
+   public Object get() {
+      if (local != shared.r) {
+         synchronized(shared) {
+            local = shared.r;
+         }
+      }
+      return local.get();
+   }
+
+   /**
+     * This method must be called by only a single thread.
+     * Set the value of the Object.
+     */
+   public void set(Object o) {
+      Reference r = new SoftReference(o);
+      local = r;
+      synchronized(shared) {
+         shared.r = local; 
+      }
+   }
+
+   /**
+     * Make a copy for another thread
+     */
+   public Object clone() {
+      try {
+         return super.clone();
+      } catch (CloneNotSupportedException e) {
+         e.printStackTrace();
+         return null;
+      }
+   }
+
+   public static void main(String arg[]) {
+
+      final SharedReference so = new SharedReference("Hello!");
+
+      Thread[] t = new Thread[20];
+      for (int i = 0; i < t.length; i++) {
+         final int num = i;
+         Thread reader = new Thread() {
+            SharedReference o = (SharedReference) so.clone();
+            public void run() {
+               Object l = o.get();
+               while (true) {
+                  Object lo = o.get();
+                  if (l != lo) {
+                     l = lo;
+                  }
+                  try {
+                     sleep(num);
+                  } catch (Exception e) {
+                     e.printStackTrace();
+                  }
+                  System.out.println(l);
+               }
+            }
+         };
+         reader.setDaemon(true);
+         reader.start();
+      }
+
+      Thread writer = new Thread() {
+         SharedReference o = (SharedReference) so.clone();
+         public void run() {
+            for (int i = 0; i < 100000; i++) {
+               try {
+                  sleep(100);
+               } catch (Exception e) {
+                  e.printStackTrace();
+               }
+               o.set(new Integer(i));
+            }
+         }
+      };
+      writer.start();
+   }
+}
