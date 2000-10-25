5de0e4665cc6b3f93bd2b0c9ac5496bf59ef8ece
brian <brian>
2000-10-25 03:35:24 +0000

    Add INDEX, FROM, and LIMIT clauses to #foreach, add error logging when we catch an exception

--- webmacro/src/org/webmacro/directive/ForeachDirective.java
+++ webmacro/src/org/webmacro/directive/ForeachDirective.java
@@ -8,20 +8,41 @@ import org.webmacro.util.*;
 
 public class ForeachDirective extends Directive {
 
-  private static final int FOREACH_TARGET = 1;
-  private static final int FOREACH_IN     = 2;
-  private static final int FOREACH_LIST   = 3;
-  private static final int FOREACH_BODY   = 4;
+  private static final int FOREACH_TARGET     = 1;
+  private static final int FOREACH_IN_K       = 2;
+  private static final int FOREACH_LIST       = 3;
+  private static final int FOREACH_BODY       = 4;
+  private static final int FOREACH_INDEXING_K = 5;
+  private static final int FOREACH_INDEX      = 6;
+  private static final int FOREACH_LIMIT_K    = 7;
+  private static final int FOREACH_LIMIT      = 8;
+  private static final int FOREACH_FROM_K     = 9;
+  private static final int FOREACH_FROM       = 10;
 
-  private Variable target;
-  private Object   list;
+  private Variable target, index;
+  private Object   list, indexFromExpr, limitExpr;
   private Macro    body;
+
+  // Syntax:
+  // #foreach list-var in list-expr 
+  //   [ limit n ] [ indexing $i [ from m ] ] 
+  // { block } 
 
   private static final ArgDescriptor[] 
     myArgs = new ArgDescriptor[] {
       new LValueArg(FOREACH_TARGET), 
-      new KeywordArg(FOREACH_IN, "in"),
+      new KeywordArg(FOREACH_IN_K, "in"),
       new RValueArg(FOREACH_LIST), 
+      new OptionChoice(2),
+        new OptionalGroup(3), 
+          new KeywordArg(FOREACH_INDEXING_K, "indexing"),
+          new LValueArg(FOREACH_INDEX),
+          new OptionalGroup(2), 
+            new KeywordArg(FOREACH_FROM_K, "from"),
+            new RValueArg(FOREACH_FROM),
+        new OptionalGroup(2),
+          new KeywordArg(FOREACH_LIMIT_K, "limit"),
+          new RValueArg(FOREACH_LIMIT),
       new BlockArg(FOREACH_BODY)
     };
 
@@ -37,24 +58,55 @@ public class ForeachDirective extends Directive {
                       BuildContext bc) 
   throws BuildException {
     target = (Variable) builder.getArg(FOREACH_TARGET, bc);
+    index  = (Variable) builder.getArg(FOREACH_INDEX, bc);
     list   = builder.getArg(FOREACH_LIST, bc);
     body   = (Block) builder.getArg(FOREACH_BODY, bc);
+    indexFromExpr = builder.getArg(FOREACH_FROM, bc);
+    limitExpr     = builder.getArg(FOREACH_LIMIT, bc);
     return this;
   }
 
   public void write(FastWriter out, Context context) 
     throws ContextException, IOException {
 
-    Object l = list;
+    Object l, limit, from;
+    long loopLimit=-1, loopStart=1, loopIndex=0;
+
+    l = list;
     while (l instanceof Macro) 
       l = ((Macro) l).evaluate(context);
-      
+
+    if (limitExpr != null) {
+      limit = limitExpr;
+      while (limit instanceof Macro)
+        limit = ((Macro) limit).evaluate(context);
+      if (Expression.isNumber(limit)) 
+        loopLimit = Expression.numberValue(limit);
+      else
+        throw new ContextException("Foreach: Cannot evaluate limit");
+    }
+
+    if (index != null && indexFromExpr != null) {
+      from = indexFromExpr;
+      while (from instanceof Macro)
+        from = ((Macro) from).evaluate(context);
+      if (Expression.isNumber(from)) 
+        loopStart = Expression.numberValue(from);
+      else
+        throw new ContextException("Foreach: Cannot evaluate loop start");
+    }
+
     try {
       if (l instanceof Object[]) {
         Object[] alist = (Object[]) l;
         for (int i = 0; i < alist.length; i++) {
           target.setValue(context, alist[i]);
+          if (index != null) 
+            index.setValue(context, new Long(loopIndex + loopStart));
           body.write(out, context);
+          ++loopIndex;
+          if (loopLimit > 0 && loopIndex >= loopLimit) 
+            break;
         }
       } else {
         Iterator iter;
@@ -65,13 +117,18 @@ public class ForeachDirective extends Directive {
         }
         while(iter.hasNext()) {
           target.setValue(context, iter.next());
+          if (index != null) 
+            index.setValue(context, new Long(loopIndex + loopStart));
           body.write(out, context);
+          ++loopIndex;
+          if (loopLimit > 0 && loopIndex >= loopLimit) 
+            break;
         }
       }
     } catch (ContextException e) {
-      //Engine.log.exception(e);
-      //Engine.log.error("unable to set a list item of list: " + _list); 
-      out.write("<!--\n Unable to resolve list " + list + " \n-->");
+      String errorText = "Foreach: Unable to set list index";
+      context.getBroker().getLog("engine").error(errorText);
+      writeWarning(errorText, out);
     }
   } 
 
