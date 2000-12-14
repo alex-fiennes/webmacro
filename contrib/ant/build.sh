14796e438d71f5e63059cf24d803fbb00a3debf0
fergus <fergus>
2000-12-14 20:26:43 +0000

    Added ant build config

---
+++ contrib/ant/build.sh
@@ -0,0 +1,25 @@
+#! /bin/sh
+
+# $Id$
+
+if [ -z "$JAVA_HOME" ]
+then
+	JAVACMD=`which java`
+	if [ -z "$JAVACMD" ]
+	then
+		echo "Cannot find JAVA. Please set your PATH."
+		exit 1
+	fi
+	JAVA_BINDIR=`dirname $JAVACMD`
+	JAVA_HOME=$JAVA_BINDIR/..
+fi
+if [ -z "$ANT_HOME" ]
+then
+    ANT_HOME=./ant
+fi
+
+JAVACMD=$JAVA_HOME/bin/java
+
+cp=$ANT_HOME/ant.jar:$ANT_HOME/parser.jar:$ANT_HOME/jaxp.jar:$JAVA_HOME/lib/tools.jar
+
+$JAVACMD -cp $cp org.apache.tools.ant.Main -f build.xml "$@"
