d4b89e500057535015bdf0fb40062400273cfe9e
fergus <fergus>
2000-12-15 11:37:13 +0000

    Ensure CLASSPATH is included in build

--- contrib/ant/build.sh
+++ contrib/ant/build.sh
@@ -22,4 +22,4 @@ JAVACMD=$JAVA_HOME/bin/java
 
 cp=$ANT_HOME/ant.jar:$ANT_HOME/parser.jar:$ANT_HOME/jaxp.jar:$JAVA_HOME/lib/tools.jar
 
-$JAVACMD -cp $cp org.apache.tools.ant.Main -f build.xml "$@"
+$JAVACMD -cp $cp:$CLASSPATH org.apache.tools.ant.Main -f build.xml "$@"
