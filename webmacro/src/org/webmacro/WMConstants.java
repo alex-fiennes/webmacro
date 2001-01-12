97f4281597ce8e16b4eda829952cfdae13bd36fe
fergus <fergus>
2001-01-12 17:24:01 +0000

    Added TemplateLocale (also a few extra comments)

--- webmacro/src/org/webmacro/WMConstants.java
+++ webmacro/src/org/webmacro/WMConstants.java
@@ -23,11 +23,22 @@ public final class WMConstants
 {
     public final static String TEMPLATE_OUTPUT_ENCODING = "TemplateOutputEncoding";
     public final static String TEMPLATE_INPUT_ENCODING = "TemplateEncoding";
+    public final static String TEMPLATE_LOCALE = "TemplateLocale";
+
     /**
-     * name of local properties file
+     * name of the local properties file.  Only limited properties are 
+     * read from this (currently to do with file encodings)
      */
     public static final String WEBMACRO_LOCAL_FILE = "WebMacro.local";
+
+    /**
+     * name of the default properties file
+     */
     public static final String WEBMACRO_DEFAULTS_FILE = "WebMacro.defaults";
+
+    /**
+     * name of the properties file
+     */
     public static final String WEBMACRO_PROPERTIES_FILE = "WebMacro.properties";
    
 }
