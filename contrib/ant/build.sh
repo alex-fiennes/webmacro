b64779cc64bbfb0d8bb5eee16ad4c07991216579
fergus <fergus>
2001-01-18 16:59:24 +0000

    - Added some comments - build to /tmp/${USER} by default

--- contrib/ant/build.sh
+++ contrib/ant/build.sh
@@ -18,8 +18,20 @@ then
     ANT_HOME=./ant
 fi
 
+#
+# where to build things by default
+# Building to /tmp is a zillion times faster on many systems 
+# (depending on how the file system is configured)
+#
+if [ -z "$WMBUILD" ]
+then 
+    WMBUILD=/tmp/${USER}/wm/
+fi
+
+
 JAVACMD=$JAVA_HOME/bin/java
 
 cp=$ANT_HOME/ant.jar:$ANT_HOME/parser.jar:$ANT_HOME/jaxp.jar:$JAVA_HOME/lib/tools.jar
 
-$JAVACMD -cp $cp:$CLASSPATH org.apache.tools.ant.Main -f build.xml "$@"
+$JAVACMD -cp $cp:$CLASSPATH org.apache.tools.ant.Main -f build.xml \
+    -Ddest=$WMBUILD/build/ -Dcontainer=$WMBUILD/tomcat/ "$@"
