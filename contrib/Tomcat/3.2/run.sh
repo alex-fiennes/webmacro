92b35d3f79d9cc0dffbd20beac27ec393e6fba36
fergus <fergus>
2001-01-18 17:00:20 +0000

    Don't do build automatically

--- contrib/Tomcat/3.2/run.sh
+++ contrib/Tomcat/3.2/run.sh
@@ -1,6 +1,4 @@
 #!/bin/ksh
-build.sh tomcat && {
-    DIR=`dirname $0`
-    export TOMCAT_HOME=/opt/jakarta-tomcat-3.2.1/
-    $TOMCAT_HOME/bin/tomcat.sh run -config $DIR/conf/server.xml
-}
+DIR=`dirname $0`
+export TOMCAT_HOME=/opt/jakarta-tomcat-3.2.1/
+$TOMCAT_HOME/bin/tomcat.sh run -config $DIR/conf/server.xml
