faa1e0a97892ff9cf8c9c37a5c4781209577cfce
brian <brian>
2001-02-08 16:20:00 +0000

    Add copyright notice

--- webmacro/src/org/webmacro/WMConstants.java
+++ webmacro/src/org/webmacro/WMConstants.java
@@ -1,20 +1,25 @@
 /*
- * Copyright (c) 1998, 1999 Semiotek Inc. All Rights Reserved.
+ * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.  
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of either of the following
+ * Open Source licenses:
  *
- * This software is the confidential intellectual property of
- * of Semiotek Inc.; it is copyrighted and licensed, not sold.
- * You may use it under the terms of the GNU General Public License,
- * version 2, as published by the Free Software Foundation. If you 
- * do not want to use the GPL, you may still use the software after
- * purchasing a proprietary developers license from Semiotek Inc.
+ * The GNU General Public License, version 2, or any later version, as
+ * published by the Free Software Foundation
+ * (http://www.fsf.org/copyleft/gpl.html);
+ *
+ *  or 
+ *
+ * The Semiotek Public License (http://webmacro.org/LICENSE.)  
  *
  * This software is provided "as is", with NO WARRANTY, not even the 
  * implied warranties of fitness to purpose, or merchantability. You
  * assume all risks and liabilities associated with its use.
  *
- * See the attached License.html file for details, or contact us
- * by e-mail at info@semiotek.com to get a copy.
+ * See www.webmacro.org for more information on the WebMacro project.  
  */
+
 
 package org.webmacro;
 
