27760ad4f8ffe1a82bba509a5e518bd6fdaed9a7
eric <eric>
2001-03-26 00:20:21 +0000

    no message

--- webmacro/test/templates/arithmetic/TestTemplate.java
+++ webmacro/test/templates/arithmetic/TestTemplate.java
@@ -1,7 +1,34 @@
 import org.webmacro.*;
 import org.webmacro.engine.*;
 
-public class TestTemplate {
+public class TestTemplate extends AbstractTemplateEvaluator {
+    
+    public void stuffContext (Context c) throws Exception
+    {
+        // fill up the context with our data
+        c.put("intn2", new Integer(-2));
+        c.put("intn1", new Integer(-1));
+        c.put("int0", new Integer(0));
+        c.put("int1", new Integer(1));
+        c.put("int2", new Integer(2));
+
+        c.put("longn2", new Long((long) -2));
+        c.put("longn1", new Long((long) -1));
+        c.put("long0", new Long((long) 0));
+        c.put("long1", new Long((long) 1));
+        c.put("long2", new Long((long) 2));
+
+        c.put("shortn2", new Short((short) -2));
+        c.put("shortn1", new Short((short) -1));
+        c.put("short0", new Short((short) 0));
+        c.put("short1", new Short((short) 1));
+        c.put("short2", new Short((short) 2));
+
+        c.put("ints", ints);
+        c.put("longs", ints);
+        c.put("shorts", ints);
+    }
+    
   public static void main(String[] args) throws Exception {
     WM wm = new WM();
     Context c = new Context(wm.getBroker());
