2aa2d623f5db77e93610be38c688c7914cd03599
e_ridge <e_ridge>
2001-03-26 07:33:16 +0000

    Test case for #parse "<template>"
    no baseline yet, as it looks like there could be a problem with whitespace before #parse.  see readme.txt

---
+++ webmacro/test/templates/parse/TestTemplate.java
@@ -0,0 +1,20 @@
+/*
+ * TestTemplate.java
+ *
+ * Created on March 25, 2001, 8:04 PM
+ */
+
+import org.webmacro.Context;
+
+/**
+ *
+ * @author  e_ridge
+ * @version 
+ */
+public class TestTemplate extends AbstractTemplateEvaluator
+{
+    public void stuffContext (Context c) throws Exception
+    {
+	// nothing to do here
+    }
+}
