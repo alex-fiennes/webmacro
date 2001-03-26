d65430f807942efd3403b138e44664b0b14c443f
eric <eric>
2001-03-26 01:06:20 +0000

    initial version of Eric's most wonderful Variable test template.

---
+++ webmacro/test/templates/variables/TestTemplate.java
@@ -0,0 +1,21 @@
+/*
+ * TestTemplate.java
+ *
+ * Created on March 25, 2001, 8:04 PM
+ */
+
+import org.webmacro.Context;
+
+/**
+ *
+ * @author  e_ridge
+ * @version 
+ */
+public class TestTemplate extends AbstractTemplateEvaluator
+{
+    public void stuffContext (Context c) throws Exception
+    {
+        
+        
+    }
+}
