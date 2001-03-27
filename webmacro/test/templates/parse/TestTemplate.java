90611db2f021c3b1d6753502ebff816b10c6d49d
eric <eric>
2001-03-27 01:27:54 +0000

    updated javadocs

--- webmacro/test/templates/parse/TestTemplate.java
+++ webmacro/test/templates/parse/TestTemplate.java
@@ -7,14 +7,17 @@
 import org.webmacro.Context;
 
 /**
- *
+ * TestTemplate for testing the #parse directive.<p>
+ * 
+ * No context is necessary here . . . yet
+ * 
  * @author  e_ridge
  * @version 
  */
 public class TestTemplate extends AbstractTemplateEvaluator
 {
-    public void stuffContext (Context c) throws Exception
-    {
-	// nothing to do here
-    }
+   public void stuffContext (Context c) throws Exception
+   {
+      // nothing to do here
+   }
 }
--- webmacro/test/templates/variables/TestTemplate.java
+++ webmacro/test/templates/variables/TestTemplate.java
@@ -7,43 +7,42 @@
 import org.webmacro.Context;
 
 /**
- *
+ * Tester for testing rouge variables
  * @author  e_ridge
  * @version 
  */
 public class TestTemplate extends AbstractTemplateEvaluator
 {
-    public static class TestObject
-    {
-        String firstName;
-        String lastName;        
-        public TestObject (String firstName, String lastName)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-        
-        public String getFirstName () { return firstName; }
-        public String getLastName () { return lastName; }
-        public String toString () { return firstName + " " + lastName; }        public String returnNull () { return null; }        public void voidMethod() { ; }
-        
-    }
-    
-    /** 
-     * the toString() method of this class returns null
-     */
-    public static class TestObject2
-    {
-       public String toString () { return null; }  
-    }
-    
-    public void stuffContext (Context c) throws Exception
-    {
-        // c.setEvaluationExceptionHandler (new org.webmacro.engine.CrankyEvaluationExceptionHandler());
-        Object nullObject = null;
-        
-        c.put ("NullObject", nullObject);
-        c.put ("TestObject", new TestObject ("Eric", "Ridge"));
-        c.put ("TestObject2", new TestObject2 ());
-    }
+   public static class TestObject
+   {
+      String firstName;
+      String lastName;                    
+      public TestObject (String firstName, String lastName)
+      {
+         this.firstName = firstName;
+         this.lastName = lastName;
+      }
+                 
+      public String getFirstName () { return firstName; }
+      public String getLastName () { return lastName; }
+      public String toString () { return firstName + " " + lastName; }      public String returnNull () { return null; }      public void voidMethod() { ; }
+                 
+   }
+             
+   /** 
+   * the toString() method of this class returns null
+   */
+   public static class TestObject2
+   {
+      public String toString () { return null; }  
+   }
+             
+   public void stuffContext (Context c) throws Exception
+   {
+      Object nullObject = null;
+                    
+      c.put ("NullObject", nullObject);
+      c.put ("TestObject", new TestObject ("Eric", "Ridge"));
+      c.put ("TestObject2", new TestObject2 ());
+   }
 }
