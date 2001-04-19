ee195f4ef5ccce9978566e88faf24704c55cb658
keats <keats>
2001-04-19 22:09:45 +0000

    initial commit

---
+++ webmacro/src/org/webmacro/servlet/TypeTool.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.  
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of either of the following
+ * Open Source licenses:
+ *
+ * The GNU General Public License, version 2, or any later version, as
+ * published by the Free Software Foundation
+ * (http://www.fsf.org/copyleft/gpl.html);
+ *
+ *  or 
+ *
+ * The Semiotek Public License (http://webmacro.org/LICENSE.)  
+ *
+ * This software is provided "as is", with NO WARRANTY, not even the 
+ * implied warranties of fitness to purpose, or merchantability. You
+ * assume all risks and liabilities associated with its use.
+ *
+ * See www.webmacro.org for more information on the WebMacro project.  
+ */
+
+package org.webmacro.servlet;
+import org.webmacro.*;
+
+/**
+ * Provide templates a way to cast objects to primitive type wrappers
+ * 
+ * @author Keats Kirsch
+ * @version 1.0
+ * @since Apr. 2001
+ * @see org.webmacro.util.CastUtil
+ */
+public class TypeTool implements ContextTool
+{
+  public Object init(Context context) 
+  {
+    return org.webmacro.util.CastUtil.getInstance();
+  }
+
+   public void destroy(Object o) { }
+}
+
+
+
