6881b48c20d5f637eb9875444b5fc612795445d1
brian <brian>
2001-04-21 00:13:21 +0000

    Add more test cases

--- webmacro/test/unit/org/webmacro/template/TestGetSet.java
+++ webmacro/test/unit/org/webmacro/template/TestGetSet.java
@@ -14,6 +14,14 @@ public class TestGetSet extends TemplateTestCase {
   public static class TestObject {
     public static int intField;
     public static long longField;
+
+    private static int intValue;
+    public static void setInt(int i) { intValue = i; }
+    public static int getInt() { return intValue; }
+
+    private static long longValue;
+    public static void setLong(long i) { longValue = i; }
+    public static long getLong() { return longValue; }
   }
 
   public static TestObject to = new TestObject();
@@ -27,6 +35,8 @@ public class TestGetSet extends TemplateTestCase {
                 new DefaultEvaluationExceptionHandler ());
 
       context.put ("TestObject", to);
+      context.put ("two", (int) 2);
+      context.put ("twoLong", (long) 2);
    }
 
    public void test1() throws Exception {
@@ -34,6 +44,29 @@ public class TestGetSet extends TemplateTestCase {
      assertStringTemplateEquals ("$TestObject.intField", "1"); 
      assertStringTemplateEquals ("#set $TestObject.intField=2", "");
      assert(to.intField == 2);
+
+     to.intField = 1;
+     assertStringTemplateEquals ("#set $TestObject.intField=$two", "");
+     assert(to.intField == 2);
+
+     to.longField = 1;
+     assertStringTemplateEquals ("$TestObject.longField", "1"); 
+     assertStringTemplateEquals ("#set $TestObject.longField=2", "");
+     assert(to.longField == 2);
+
+     to.longField = 1;
+     assertStringTemplateEquals ("$TestObject.longField", "1"); 
+     assertStringTemplateEquals ("#set $TestObject.longField=$twoLong", "");
+     assert(to.longField == 2);
+
+     to.setInt(1);
+     assertStringTemplateEquals ("$TestObject.Int", "1"); 
+     assertStringTemplateEquals ("#set $TestObject.Int=2", "");
+     assert(to.getInt() == 2);
+
+     to.setInt(1);
+     assertStringTemplateEquals ("$TestObject.Int", "1"); 
+     assertStringTemplateEquals ("#set $TestObject.Int=$two", "");
+     assert(to.getInt() == 2);
    }
-   
 }
