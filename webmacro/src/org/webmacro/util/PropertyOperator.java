35e044adeee94093ddaf07f31a33b0413f33e3a4
brian <brian>
2001-04-27 01:18:12 +0000

    Use smaller ScalableMap for PropertyOperator cache

--- webmacro/src/org/webmacro/util/PropertyOperator.java
+++ webmacro/src/org/webmacro/util/PropertyOperator.java
@@ -165,7 +165,7 @@ final public class PropertyOperator
    /**
      * Private cache of all the property operators constructed so far
      */
-   static private ScalableMap _operators = new ScalableMap(10001);
+   static private ScalableMap _operators = new ScalableMap(1001);
 
    /**
      * Find the PropertyOperator that knows about this type of object
