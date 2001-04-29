dc780e04e37793d332df503ed246beca0bae77ee
eric <eric>
2001-04-29 22:19:28 +0000

    test3() and test4():  can't pass a null as a parameter to a method:
    $Object.callMethod("param1", null)  --> fails
    
            and
    
    #set $myNull = null
    $Object.callMethod("param1", $myNull) --> fails

--- webmacro/test/unit/org/webmacro/template/TestGetSet.java
+++ webmacro/test/unit/org/webmacro/template/TestGetSet.java
@@ -1,6 +1,8 @@
 package org.webmacro.template;
 
 import java.io.*;
+import java.util.Map;
+import java.util.HashMap;
 import org.webmacro.*;
 import org.webmacro.engine.StringTemplate;
 import org.webmacro.engine.DefaultEvaluationExceptionHandler;
@@ -14,6 +16,8 @@ public class TestGetSet extends TemplateTestCase {
   public static class TestObject {
     public static int intField;
     public static long longField;
+    private Map map = new HashMap ();
+    private Object obj = null;
 
     private static int intValue;
     public static void setInt(int i) { intValue = i; }
@@ -22,6 +26,18 @@ public class TestGetSet extends TemplateTestCase {
     private static long longValue;
     public static void setLong(long i) { longValue = i; }
     public static long getLong() { return longValue; }
+   
+    public void put (String key, Object value) {
+       map.put (key, value);
+    }
+
+    public Object get (String key) {
+       return map.get (key);
+    }
+ 
+    public void setObjectValue (Object value) {  
+       this.obj = value;
+    }
   }
 
   public static TestObject to = new TestObject();
@@ -37,6 +53,11 @@ public class TestGetSet extends TemplateTestCase {
       context.put ("TestObject", to);
       context.put ("two", (int) 2);
       context.put ("twoLong", (long) 2);
+
+      TestObject to2 = new TestObject ();
+      to2.intField = 1;
+      to2.put ("Value", new TestObject ());
+      context.put ("TestObject2", to2);
    }
 
    public void test1() throws Exception {
@@ -68,5 +89,26 @@ public class TestGetSet extends TemplateTestCase {
      assertStringTemplateEquals ("$TestObject.Int", "1"); 
      assertStringTemplateEquals ("#set $TestObject.Int=$two", "");
      assert(to.getInt() == 2);
+   }
+
+   /** Using WM shorthand syntax, get an object from
+    * a map and call a method on it
+    */
+   public void test2 () throws Exception {
+      String tmpl = "#if($TestObject2.Value.getInt() > 0){pass}#else{fail}";
+      assertStringTemplateEquals (tmpl, "pass"); 
+   }
+
+   /** pass a null as a parameter to a method */
+   public void test3 () throws Exception {
+      String tmpl = "$TestObject2.setObjectValue(null)";
+      assertStringTemplateEquals (tmpl, "");
+   }
+
+   /** same as test3, but use the null via a variable reference */
+   public void test4 () throws Exception {
+      String tmpl = "#set $foo = null\n"
+                  + "$TestObject2.setObjectValue($foo)";
+      assertStringTemplateEquals (tmpl, "");
    }
 }
