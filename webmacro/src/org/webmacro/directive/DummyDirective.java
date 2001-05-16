43d099c3361400a22cec42c4b0f938643f046d0a
keats <keats>
2001-05-16 04:36:08 +0000

    *** empty log message ***

---
+++ webmacro/src/org/webmacro/directive/DummyDirective.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.  
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of either of the following
+ * Open Source licenses:
+ *
+ * The GNU General Public License, version 2, or any later version, as
+ * published by the Free Software Foundation
+ * (http://www.fsf.org/copyleft/gpl.html);
+ *
+ *  or 
+ *
+ * The Semiotek Public License (http://webmacro.org/LICENSE.)  
+ *
+ * This software is provided "as is", with NO WARRANTY, not even the 
+ * implied warranties of fitness to purpose, or merchantability. You
+ * assume all risks and liabilities associated with its use.
+ *
+ * See www.webmacro.org for more information on the WebMacro project.  
+ */
+
+package org.webmacro.directive;
+
+import java.io.*;
+import org.webmacro.*;
+import org.webmacro.engine.*;
+
+public class DummyDirective extends Directive {
+
+  private static final ArgDescriptor[] 
+    myArgs = new ArgDescriptor[] {
+    };
+
+  private static final DirectiveDescriptor 
+    myDescr = new DirectiveDescriptor("dummy", null, myArgs, null);
+
+  public static DirectiveDescriptor getDescriptor() {
+    return myDescr;
+  }
+
+  public Object build(DirectiveBuilder builder, 
+                      BuildContext bc) 
+  throws BuildException {
+    return null;
+  }
+
+  public void write(FastWriter out, Context context) 
+    throws PropertyException, IOException {
+  } 
+
+  public void accept(TemplateVisitor v) {
+    v.beginDirective(myDescr.name);
+    v.endDirective();
+  }
+
+}
