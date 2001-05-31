d8ce355276175ac2c23284044b9c2dcc4345fce3
brian <brian>
2001-05-31 02:28:31 +0000

    Fix bug 25 (inline lists); turned out to be a duplicate of bug 30 (whitespace after paren)

---
+++ webmacro/test/unit/org/webmacro/template/TestList.java
@@ -0,0 +1,41 @@
+package org.webmacro.template;
+
+import java.io.*;
+import java.util.Map;
+import java.util.HashMap;
+import org.webmacro.*;
+import org.webmacro.engine.StringTemplate;
+import org.webmacro.engine.DefaultEvaluationExceptionHandler;
+
+import junit.framework.*;
+
+public class TestList extends TemplateTestCase {
+
+  public static class Dumper {
+    public String print(Object[] objects) {
+      String s="";
+      for (int i=0; i<objects.length; i++)
+        s += objects[i].toString() + " ";
+      return s;
+    }
+  }
+
+  public static Dumper d = new Dumper();
+
+   public TestList (String name) {
+      super (name);
+   }
+
+   public void stuffContext (Context context) throws Exception {
+      context.setEvaluationExceptionHandler (
+                new DefaultEvaluationExceptionHandler ());
+
+      context.put ("d", d);
+   }
+
+   public void test1() throws Exception {
+     assertStringTemplateEquals("#set $a=[ 1, 2, 3 ] $d.print($a)", "1 2 3 ");
+     assertStringTemplateEquals("$d.print([ 1, 2, 3 ])", "1 2 3 ");
+   }
+
+}
