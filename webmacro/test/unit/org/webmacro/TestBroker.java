e002e5641e3f7d8c6104e8708bde8957126e252a
eric <eric>
2001-06-18 04:45:07 +0000

    A test case for testing some of the basic functionality of the Broker. passes 100%.  although WM log output gets sent to stdout.  How did we turn that off for the template test cases?

---
+++ webmacro/test/unit/org/webmacro/TestBroker.java
@@ -0,0 +1,61 @@
+package org.webmacro;
+
+import java.io.*;
+
+import org.webmacro.*;
+import org.webmacro.util.*;
+
+import junit.framework.*;
+
+/* Test various features of a default-configured Broker */
+public class TestBroker extends TestCase {
+  private WebMacro _wm;
+  private Broker _broker;
+
+  public TestBroker(String name) {
+    super(name);
+  }
+
+  protected void setUp() {
+     try {
+       _wm = new WM ();
+     } catch (Exception e) {
+       System.err.println ("Could not initialize WebMacro!");
+     }
+     _broker = _wm.getBroker ();
+  }
+  
+  public void testBrokerLocal () throws Exception {
+     _broker.setBrokerLocal ("MyName", "Mud");
+
+     assert (_broker.getBrokerLocal ("MyName")
+                    .toString().equals ("Mud"));
+  }
+
+  public void testGetSettings() throws Exception {
+     Settings s = _broker.getSettings();
+     assert (s != null);
+     assert (_broker.getSetting("ErrorTemplate")
+                    .equals ("error.wm"));
+  }
+
+  public void testGetProvider() throws Exception {
+     Provider p = _broker.getProvider ("template");
+     assert (p != null);
+  }
+
+  public void testGetLog() throws Exception {
+     Log l = _broker.getLog ("template");
+     assert (l != null);
+
+     l = _broker.getLog ("NewLog");
+     assert (l != null);
+  }
+
+  public void testGetResource() throws Exception {
+     java.net.URL url = _broker.getResource ("WebMacro.defaults");
+     assert (url != null);
+  }
+}
+
+
