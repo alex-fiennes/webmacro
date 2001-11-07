be184bf06851aa77f47368f2090f0490fac5290f
brian <brian>
2001-11-07 05:01:06 +0000

    Add more test cases

--- webmacro/test/unit/org/webmacro/template/TestDirectiveParser.java
+++ webmacro/test/unit/org/webmacro/template/TestDirectiveParser.java
@@ -34,7 +34,15 @@ public class TestDirectiveParser extends TemplateTestCase {
     dp.register(clazz, name);
   }
 
-  public static class DirectiveOne extends Directive {
+  private static class BaseDirective extends Directive {
+    public Object build(DirectiveBuilder builder, BuildContext bc) { 
+      return null; 
+    }
+    
+    public void write(FastWriter out, Context context) {}
+  }
+
+  public static class DirectiveOne extends BaseDirective {
 
     private static final ArgDescriptor[] 
       myArgs = new ArgDescriptor[] {
@@ -52,17 +60,9 @@ public class TestDirectiveParser extends TemplateTestCase {
     public static DirectiveDescriptor getDescriptor() {
       return myDescr;
     }
-
-    public DirectiveOne() {}
-    
-    public Object build(DirectiveBuilder builder, BuildContext bc) { 
-      return null; 
-    }
-    
-    public void write(FastWriter out, Context context) {}
   }
 
-  public void testKeywords() throws Exception {
+  public void testOne() throws Exception {
     registerDirective("one", 
                       "org.webmacro.template.TestDirectiveParser$DirectiveOne");
     assertStringTemplateEquals("#one one two", "");
@@ -71,4 +71,81 @@ public class TestDirectiveParser extends TemplateTestCase {
     assertStringTemplateEquals("#one one two three four", "");
     assertStringTemplateMatches("#one one three", "^<!--.*failed.*");
   }
+
+
+  public static class DirectiveTwo extends BaseDirective {
+
+    private static final ArgDescriptor[] 
+      myArgs = new ArgDescriptor[] {
+        new SingleOptionChoice(4),
+        new OptionalGroup(1),
+        new KeywordArg(1, "global"),
+        new OptionalGroup(1),
+        new KeywordArg(2, "application"),
+        new OptionalGroup(1),
+        new KeywordArg(3, "session"),
+        new OptionalGroup(1),
+        new KeywordArg(4, "page"),
+          };
+
+    private static final DirectiveDescriptor 
+      myDescr = new DirectiveDescriptor(null, null, myArgs, null);
+
+    public static DirectiveDescriptor getDescriptor() {
+      return myDescr;
+    }
+  }
+
+  public void testTwo() throws Exception {
+    registerDirective("two", 
+                      "org.webmacro.template.TestDirectiveParser$DirectiveTwo");
+    assertStringTemplateEquals("#two global", "");
+    assertStringTemplateEquals("#two global application", "application");
+    assertStringTemplateEquals("#two application", "");
+    assertStringTemplateEquals("#two session", "");
+    assertStringTemplateEquals("#two page", "");
+    assertStringTemplateMatches("#two bears", "bears");
+    assertStringTemplateMatches("#two", "");
+  }
+
+  public static class DirectiveThree extends BaseDirective {
+
+    private static final ArgDescriptor[] 
+      myArgs = new ArgDescriptor[] {
+        new OptionChoice(4),
+        new OptionalGroup(1),
+        new KeywordArg(1, "global"),
+        new OptionalGroup(1),
+        new KeywordArg(2, "application"),
+        new OptionalGroup(1),
+        new KeywordArg(3, "session"),
+        new OptionalGroup(1),
+        new KeywordArg(4, "page"),
+          };
+
+    private static final DirectiveDescriptor 
+      myDescr = new DirectiveDescriptor(null, null, myArgs, null);
+
+    public static DirectiveDescriptor getDescriptor() {
+      return myDescr;
+    }
+  }
+
+  public void testThree() throws Exception {
+    registerDirective("three", 
+                      "org.webmacro.template.TestDirectiveParser$DirectiveThree");
+    assertStringTemplateEquals("#three", "");
+    assertStringTemplateEquals("#three global", "");
+    assertStringTemplateEquals("#three global application", "");
+    assertStringTemplateEquals("#three global session", "");
+    assertStringTemplateEquals("#three global page", "");
+    assertStringTemplateEquals("#three application", "");
+    assertStringTemplateEquals("#three session", "");
+    assertStringTemplateEquals("#three page", "");
+    assertStringTemplateEquals("#three page global", "");
+    assertStringTemplateEquals("#three page page global", "");
+    assertStringTemplateEquals("#three page page global application", "");
+    assertStringTemplateEquals("#three page page global page application page", "");
+  }
 }
+
