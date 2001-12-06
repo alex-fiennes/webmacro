2521c4cb2cc30a1d46b7e73ed5af1faf08d127b4
keats <keats>
2001-12-06 23:20:52 +0000

    Initial commit.  A singleton to be returned as representation of an undefined value in a template.

---
+++ webmacro/src/org/webmacro/engine/UndefinedMacro.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 1998-2001 Semiotek Inc.  All Rights Reserved.  
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of either of the following
+ * Open Source licenses:
+ *
+ * The GNU General Public License, version 2, or any later version, as
+ * published by the Free Software Foundation
+ * (http://www.fsf.org/copyleft/gpl.html);
+ *
+ *  or 
+ *
+ * The Semiotek Public License (http://webmacro.org/LICENSE.)  
+ *
+ * This software is provided "as is", with NO WARRANTY, not even the 
+ * implied warranties of fitness to purpose, or merchantability. You
+ * assume all risks and liabilities associated with its use.
+ *
+ * See www.webmacro.org for more information on the WebMacro project.  
+ */
+
+package org.webmacro.engine;
+
+import java.io.*;
+import org.webmacro.*;
+import org.webmacro.util.*;
+
+/**
+  * Looks like a Macro, but really it's an undefined variable.
+  */
+public final class UndefinedMacro implements Macro, Visitable
+{
+
+   final static private UndefinedMacro _singleton = new UndefinedMacro();
+
+   private UndefinedMacro(){
+   }
+   
+   static public UndefinedMacro getInstance(){ return _singleton; }
+
+   public final String toString() {
+      return "--undefined--";
+   }
+
+   public void accept(TemplateVisitor v) { v.visitString(toString()); } 
+
+   /**
+     * Returns the wrapped object, context is ignored.
+     */
+   public final Object evaluate(Context context) {
+      return _singleton;
+   }
+   
+   public Object get(Object key){
+       return _singleton;
+   }
+
+   /**
+     * Throws an exception -- cannot write an undefined
+     */
+   public final void write(FastWriter out, Context context) 
+      throws PropertyException
+   {
+       throw new PropertyException("Attempted to write an undefined value");       
+   }
+}
