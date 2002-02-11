c397c2efa11029249ca2a9bbfdfda952da07658c
eric <eric>
2002-02-11 20:41:41 +0000

    add ability to get the number of args from the directive builder, along with accessing an _exact_ argument (regardless of arg id).

--- webmacro/src/org/webmacro/directive/ArgsHolder.java
+++ webmacro/src/org/webmacro/directive/ArgsHolder.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.  
- * 
+ * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted under the terms of either of the following
  * Open Source licenses:
@@ -9,15 +9,15 @@
  * published by the Free Software Foundation
  * (http://www.fsf.org/copyleft/gpl.html);
  *
- *  or 
+ *  or
  *
- * The Semiotek Public License (http://webmacro.org/LICENSE.)  
+ * The Semiotek Public License (http://webmacro.org/LICENSE.)
  *
- * This software is provided "as is", with NO WARRANTY, not even the 
+ * This software is provided "as is", with NO WARRANTY, not even the
  * implied warranties of fitness to purpose, or merchantability. You
  * assume all risks and liabilities associated with its use.
  *
- * See www.webmacro.org for more information on the WebMacro project.  
+ * See www.webmacro.org for more information on the WebMacro project.
  */
 
 package org.webmacro.directive;
@@ -30,7 +30,7 @@ import org.webmacro.engine.*;
  * ArgsHolder is a container for directive arguments.  The parser creates
  * and populates the ArgsHolder based on the contents of the directive
  * descriptor.  The directive, in the build() method, will call the getArg()
- * methods to retrieve the arguments and build the directive.  
+ * methods to retrieve the arguments and build the directive.
  * @author Brian Goetz
  */
 
@@ -41,7 +41,7 @@ public final class ArgsHolder implements DirectiveArgs
 
   // The parser will call setArg(), and we'll store them here.  For
   // each argument in the argument list, the corresponding argument data
-  // will be in the same index into buildArgs.  
+  // will be in the same index into buildArgs.
   private Object[] buildArgs;
 
 
@@ -50,7 +50,7 @@ public final class ArgsHolder implements DirectiveArgs
     buildArgs = new Object[args.length];
   }
 
-  private final int findArgIndex(int id) 
+  private final int findArgIndex(int id)
     throws BuildException {
     for (int i=0; i<args.length; i++) {
       if (args[i].id == id)
@@ -59,25 +59,36 @@ public final class ArgsHolder implements DirectiveArgs
     throw new BuildException("Invalid argument ID " + id + " requested ");
   }
 
+  public Object getExactArg (int idx) throws BuildException {
+     return buildArgs[idx];
+  }
+
   /**
-   * Retrieve the argument whose id is the specified id.  
+   * How many arguments does this ArgsHolder have?
    */
-  public final Object getArg(int id) 
+  public final int getArgCount () {
+    return buildArgs.length;
+  }
+
+  /**
+   * Retrieve the argument whose id is the specified id.
+   */
+  public final Object getArg(int id)
     throws BuildException {
     int index = findArgIndex(id);
     return buildArgs[index];
   }
 
   /**
-   * Retrieve the argument whose id is the specified id, and if it is a 
+   * Retrieve the argument whose id is the specified id, and if it is a
    * Builder, build it with the specified build context.
    */
-  public final Object getArg(int id, BuildContext bc) 
+  public final Object getArg(int id, BuildContext bc)
     throws BuildException {
     int index = findArgIndex(id);
 
     Object o = buildArgs[index];
-    return (o instanceof Builder) 
+    return (o instanceof Builder)
       ? ((Builder) o).build(bc)
       : o;
   }
@@ -85,10 +96,10 @@ public final class ArgsHolder implements DirectiveArgs
   /**
    * Set the argument whose id is the specified id.  If the argument has
    * already been set, it is overwritten.  Generally not used by directives,
-   * only used by the parser. 
+   * only used by the parser.
    */
 
-  public final void setArg(int id, Object o) 
+  public final void setArg(int id, Object o)
     throws BuildException {
     int index = findArgIndex(id);
     buildArgs[index] = o;
--- webmacro/src/org/webmacro/directive/DirectiveBuilder.java
+++ webmacro/src/org/webmacro/directive/DirectiveBuilder.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.  
- * 
+ * Copyright (C) 1998-2000 Semiotek Inc.  All Rights Reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted under the terms of either of the following
  * Open Source licenses:
@@ -9,15 +9,15 @@
  * published by the Free Software Foundation
  * (http://www.fsf.org/copyleft/gpl.html);
  *
- *  or 
+ *  or
  *
- * The Semiotek Public License (http://webmacro.org/LICENSE.)  
+ * The Semiotek Public License (http://webmacro.org/LICENSE.)
  *
- * This software is provided "as is", with NO WARRANTY, not even the 
+ * This software is provided "as is", with NO WARRANTY, not even the
  * implied warranties of fitness to purpose, or merchantability. You
  * assume all risks and liabilities associated with its use.
  *
- * See www.webmacro.org for more information on the WebMacro project.  
+ * See www.webmacro.org for more information on the WebMacro project.
  */
 
 package org.webmacro.directive;
@@ -28,9 +28,9 @@ import org.webmacro.engine.*;
 
 /**
  * DirectiveBuilder manages the building of directives.  It is created
- * by the parser, which populates it with the directive arguments, and 
+ * by the parser, which populates it with the directive arguments, and
  * the DirectiveBuilder.build() method calls the build() method for the
- * appropriate directive.  
+ * appropriate directive.
  * @author Brian Goetz
  */
 
@@ -49,8 +49,8 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
   private static final ArgsHolder[] aha = new ArgsHolder[0];
   private static final String
     EXC_GETSUBD_NOTREPEATING = "getSubdirective: attempt to get repeating "
-      + "subdirective -- use getRepeatingSubdirective() instead", 
-    EXC_GETSUBD_REPEATING = "getRepeatingSubdirective: attempt to get " 
+      + "subdirective -- use getRepeatingSubdirective() instead",
+    EXC_GETSUBD_REPEATING = "getRepeatingSubdirective: attempt to get "
       + "nonrepeating subdirective -- use getSubdirective() instead";
 
   public DirectiveBuilder(DirectiveDescriptor desc) {
@@ -63,32 +63,49 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
       subdirectives = new Object[desc.subdirectives.length];
   }
 
-  private int findSubdirectiveIndex(int id) 
+  private int findSubdirectiveIndex(int id)
     throws BuildException {
     for (int i=0; i<desc.subdirectives.length; i++) {
       if (desc.subdirectives[i].id == id)
         return i;
     }
-    throw new BuildException("Invalid argument ID " + id 
+    throw new BuildException("Invalid argument ID " + id
                              + " requested for directive " + desc.name);
   }
 
+  public Object getExactArg (int idx) throws BuildException {
+      if (buildArgs == null)
+        return null;
+      else
+        return buildArgs.getExactArg(idx);
+  }
+
   /**
-   * Retrieve the argument whose id is the specified id.  
+   * How many arguments does this builder have?
    */
-  public Object getArg(int argId) 
+  public int getArgCount() {
+     if (buildArgs == null)
+         return 0;
+      else
+         return buildArgs.getArgCount();
+  }
+
+  /**
+   * Retrieve the argument whose id is the specified id.
+   */
+  public Object getArg(int argId)
     throws BuildException {
     if (buildArgs == null)
       return null;
-    else 
+    else
       return buildArgs.getArg(argId);
   }
 
   /**
-   * Retrieve the argument whose id is the specified id, and if it is a 
+   * Retrieve the argument whose id is the specified id, and if it is a
    * Builder, build it with the specified build context.
    */
-  public Object getArg(int argId, BuildContext bc) 
+  public Object getArg(int argId, BuildContext bc)
     throws BuildException {
     if (buildArgs == null)
       return null;
@@ -97,15 +114,15 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
   }
 
   /**
-   * Find out how many subdirectives of the specified id were present.  
+   * Find out how many subdirectives of the specified id were present.
    */
-  public int getSubdirectiveCount(int subdId) 
+  public int getSubdirectiveCount(int subdId)
     throws BuildException {
     int i = findSubdirectiveIndex(subdId);
     if (subdirectives[i] == null)
       return 0;
-    else 
-      return (desc.subdirectives[i].repeating) 
+    else
+      return (desc.subdirectives[i].repeating)
         ? ((ArgsHolder[]) (subdirectives[i])).length
         : 1;
   }
@@ -113,25 +130,25 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
   /**
    * Set the argument whose id is the specified id.  If the argument has
    * already been set, it is overwritten.  Generally not used by directives,
-   * only used by the parser. 
+   * only used by the parser.
    */
-  public void setArg(int argId, Object arg) 
+  public void setArg(int argId, Object arg)
   throws BuildException {
     buildArgs.setArg(argId, arg);
   }
 
   /**
    * Create a new subdirective of the specified id and create an ArgsHolder
-   * for its arguments.  Not used by directives.  
+   * for its arguments.  Not used by directives.
    */
-  public DirectiveArgs newSubdirective(int subdId) 
+  public DirectiveArgs newSubdirective(int subdId)
   throws BuildException {
     ArgsHolder ah = null;
     int index = findSubdirectiveIndex(subdId);
     if (!desc.subdirectives[index].repeating) {
-      if (subdirectives[index] == null) 
+      if (subdirectives[index] == null)
         subdirectives[index] = new ArgsHolder(desc.subdirectives[index].args);
-      ah = (ArgsHolder) subdirectives[index]; 
+      ah = (ArgsHolder) subdirectives[index];
     }
     else {
       Vector v;
@@ -145,12 +162,12 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
     return ah;
   }
 
-  /** 
+  /**
    * Check to see if the specified subdirective is OK at this point.
    * The only reason it wouldn't be is because we've already got one
    * and its not a repeating subdirective
    */
-  public boolean subdirectiveOk(int subdId) throws BuildException { 
+  public boolean subdirectiveOk(int subdId) throws BuildException {
     int index = findSubdirectiveIndex(subdId);
     return desc.subdirectives[index].repeating || subdirectives[index] == null;
   }
@@ -158,12 +175,12 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
   /**
    * Retrieves the ArgsHolder for the associated subdirective so that the
    * subdirective arguments can be retrieved.  Only valid if the specified
-   * subdirective is not repeating. 
+   * subdirective is not repeating.
    */
-  public ArgsHolder getSubdirective(int subdId) 
+  public ArgsHolder getSubdirective(int subdId)
     throws BuildException {
     int index = findSubdirectiveIndex(subdId);
-    if (desc.subdirectives[index].repeating) 
+    if (desc.subdirectives[index].repeating)
       throw new BuildException(EXC_GETSUBD_NOTREPEATING);
     return (ArgsHolder) subdirectives[index];
   }
@@ -171,12 +188,12 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
   /**
    * Retrieves an array of ArgsHolders for the associated subdirective
    * so that the subdirective arguments can be retrieved.  Only valid
-   * if the specified subdirective is repeating.  
+   * if the specified subdirective is repeating.
    */
-  public ArgsHolder[] getRepeatingSubdirective(int subdId) 
+  public ArgsHolder[] getRepeatingSubdirective(int subdId)
     throws BuildException {
     int index = findSubdirectiveIndex(subdId);
-    if (!desc.subdirectives[index].repeating) 
+    if (!desc.subdirectives[index].repeating)
       throw new BuildException(EXC_GETSUBD_REPEATING);
     if (subdirectives[index] == null)
       return null;
@@ -191,15 +208,15 @@ public final class DirectiveBuilder implements Builder, DirectiveArgs
     Directive d;
     try {
       d = (Directive) desc.dirClass.newInstance();
-    } catch (Exception e) { 
-      throw new BuildException("Error instantiating Directive object for #" 
+    } catch (Exception e) {
+      throw new BuildException("Error instantiating Directive object for #"
                                + desc.name);
     };
     return d.build(this, bc);
   }
 
   /**
-   * Get the name this directive was registered as 
+   * Get the name this directive was registered as
    */
   public String getName() {
     return desc.name;
