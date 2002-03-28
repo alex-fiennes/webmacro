9e09aa3e00238a3a9199297b354a4284afbaeb6a
eric <eric>
2002-03-28 04:03:01 +0000

    updated test case to test #include as macro.  everything still seems to work

--- webmacro/test/unit/org/webmacro/template/TestParseInclude.java
+++ webmacro/test/unit/org/webmacro/template/TestParseInclude.java
@@ -1,13 +1,7 @@
 package org.webmacro.template;
 
-import java.io.*;
-import org.webmacro.*;
-import org.webmacro.engine.StringTemplate;
+import org.webmacro.Context;
 import org.webmacro.engine.DefaultEvaluationExceptionHandler;
-
-import junit.framework.*;
-
-import org.apache.regexp.RE;
 
 
 /**
@@ -16,55 +10,69 @@ import org.apache.regexp.RE;
  *
  * The file /etc/password should exist on your
  * filesystem and contain "root:" somewhere in it
- * 
+ *
  * And your computer will need access to http://www.webmacro.org/
  */
 public class TestParseInclude extends TemplateTestCase {
 
-   public TestParseInclude (String name) {
-      super (name);
-   }
+    public TestParseInclude(String name) {
+        super(name);
+    }
 
-   public void stuffContext (Context context) throws Exception {
-      context.setEvaluationExceptionHandler (
-                new DefaultEvaluationExceptionHandler ());
+    public void stuffContext(Context context) throws Exception {
+        context.setEvaluationExceptionHandler(
+                new DefaultEvaluationExceptionHandler());
 
-      context.put ("TemplateName", "org/webmacro/template/test_parse.wm");
-   }
+        context.put("TemplateName", "org/webmacro/template/test_parse.wm");
+    }
 
-   public void testParse () throws Exception {
-      // parse in "test_parse.wm"
-      //   contains only the word "pass"
-      assertStringTemplateMatches ("#parse \"org/webmacro/template/test_parse.wm\"", 
-                                  "pass"); 
+    public void testParse() throws Exception {
+        // parse in "test_parse.wm"
+        //   contains only the word "pass"
+        assertStringTemplateMatches("#parse \"org/webmacro/template/test_parse.wm\"",
+                "pass");
 
-      assertStringTemplateMatches ("#parse \"$TemplateName\"", 
-                                  "pass"); 
+        assertStringTemplateMatches("#parse \"$TemplateName\"",
+                "pass");
 
-      // parse the same template using a variable
-      assertStringTemplateMatches ("#parse \"$TemplateName\"",
-                                 "pass");
-   }
+        // parse the same template using a variable
+        assertStringTemplateMatches("#parse \"$TemplateName\"",
+                "pass");
+    }
 
-   /** test a template that is greater than 2k in size */
-   public void testBigTemplate () throws Exception {
-      // read in a big template 
-      // and trim off whitespace to make matching easier
-      String tmpl = templateFileToString ("org/webmacro/template/big_template.wm");
-      tmpl = tmpl.trim ();
+    /** test a template that is greater than 2k in size */
+    public void testBigTemplate() throws Exception {
+        // read in a big template
+        // and trim off whitespace to make matching easier
+        String tmpl = templateFileToString("org/webmacro/template/big_template.wm");
+        tmpl = tmpl.trim();
 
-      assertStringTemplateMatches (tmpl, "pass$");
-   }
+        assertStringTemplateMatches(tmpl, "pass$");
+    }
 
-   public void testInclude () throws Exception {
-      // include /etc/password
-      assertStringTemplateMatches ("#include \"/etc/passwd\"", 
-                                   ".*root:.*");
+    public void testInclude() throws Exception {
+        // include /etc/password
+        assertStringTemplateMatches("#include \"/etc/passwd\"",
+                ".*root:.*");
 
-      // include http://www.yahoo.com/
-      assertStringTemplateMatches ("#include \"http://www.webmacro.org\"",
-                                   ".*WebMacro.*");
+        // include http://www.yahoo.com/
+        assertStringTemplateMatches("#include \"http://www.webmacro.org\"",
+                ".*WebMacro.*");
 
-   }
+    }
 
+    public void testIncludeAsMacro() throws Exception {
+        assertStringTemplateMatches("#include as macro \"org/webmacro/template/macros.wm\"\n"
+                            + "#foo1()", "foo1");
+
+        assertStringTemplateMatches("#include as macro \"org/webmacro/template/macros.wm\"\n"
+                            + "$a", "\\$a was found");
+
+    }
+
+    public static void main (String[] args) throws Exception {
+        TestParseInclude t = new TestParseInclude("test");
+        t.setUp();
+        t.testIncludeAsMacro();
+    }
 }
