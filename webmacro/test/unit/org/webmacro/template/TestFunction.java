2eac518d38e24e9cfbf27ea3617199652b7dfe6c
keats <keats>
2003-03-27 05:09:58 +0000

    added tests for functions that return null or void.

--- webmacro/test/unit/org/webmacro/template/TestFunction.java
+++ webmacro/test/unit/org/webmacro/template/TestFunction.java
@@ -19,12 +19,28 @@ public class TestFunction extends TemplateTestCase {
       return num % modulus;
    }
    
+   public void voidFunc1(){
+   }
+   
+   public void voidFunc2(String dummyArg){
+      if (dummyArg == null) throw new RuntimeException();
+   }
+   
+   public Object nullFunc(){
+      return null;
+   }
+   
    public void stuffContext (Context context) throws Exception {
+      context.setEvaluationExceptionHandler(
+         new org.webmacro.engine.CrankyEvaluationExceptionHandler());
       context.putFunction("toList", java.util.Arrays.class, "asList");
       context.putGlobalFunction("escape", org.webmacro.util.HTMLEscaper.class, "escape");
       context.putGlobalFunction("mod", this, "modFunc");
       String abcs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
       context.putFunction("mySubstr", abcs, "substring");
+      context.putFunction("voidFunc1", this, "voidFunc1");
+      context.putFunction("voidFunc2", this, "voidFunc2");
+      context.putFunction("nullFunc", this, "nullFunc");
    }
 
    public void testFunctionToList() throws Exception {
@@ -42,5 +58,12 @@ public class TestFunction extends TemplateTestCase {
    public void testSubstringFunction() throws Exception {
        assertStringTemplateEquals("$mySubstr(24)", "YZ");
        assertStringTemplateEquals("$mySubstr(0,3)", "ABC");
+   }
+   
+   public void testVoidFunction() throws Exception {
+      assertStringTemplateEquals("$voidFunc1()", "");
+      assertStringTemplateEquals("$voidFunc2('bogus arg')", "");
+      assertStringTemplateEquals("#if ($nullFunc()==null){true} #else {false}", "true");
+      assertStringTemplateThrows("$nullFunc()", org.webmacro.PropertyException.NullValueException.class);
    }
 }
