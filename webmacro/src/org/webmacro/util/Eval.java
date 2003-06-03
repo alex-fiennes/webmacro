a10e88f6ee68c21a06b384de43db9b29aad7aaae
keats <keats>
2003-06-03 05:21:51 +0000

    Provides a WMScript function, $eval(), that does nothing but provide a holder that allows an expression to be evaluated and expanded into the template.

---
+++ webmacro/src/org/webmacro/util/Eval.java
@@ -0,0 +1,26 @@
+package org.webmacro.util;
+
+/*
+ * Eval.java - static class with one method eval which just returns its
+ * argument.  This is a workaround for a WebMacro syntax issue, where an
+ * expression isn't recognized properly outside of a directive or method call.
+ * The eval() function can be configured in the WebMacro.properties file, e.g.:
+ *    functions.eval=org.webmacro.util.Eval.eval
+ *
+ * Created on May 6, 2003, 12:51 AM
+ */
+
+/**
+ *
+ * @author  Keats
+ */
+public class Eval {
+   
+   /** Private constructor for a static class */
+   private Eval() {
+   }
+   
+   static public Object eval(Object o){
+      return o;
+   }
+}
