4a95e190c9b2996ba6eefa8d10c525603e875476
wangjammer5 <wangjammer5>
2003-07-16 14:24:42 +0000

    Added another test of double-binary dereferencing.

--- webmacro/test/unit/org/webmacro/template/TestBinaryAccessor.java
+++ webmacro/test/unit/org/webmacro/template/TestBinaryAccessor.java
@@ -22,9 +22,9 @@ public class TestBinaryAccessor extends TemplateTestCase
 
         private Map params = new Hashtable();
 
-        public String getParameters( String key )
+        public Object getParameters( String key )
         {
-            return (String) params.get( key );
+            return params.get( key );
         }
 
         public void setParameters( String key, Object value )
@@ -35,26 +35,49 @@ public class TestBinaryAccessor extends TemplateTestCase
 
     public void testBinaryGet()
     {
-        assertStringTemplateEquals("$obj.Parameters.Name", "Marc");
+        assertStringTemplateEquals("$obj1.Parameters.Name", "Marc");
     }
 
     public void testBinarySet()
     {
-        assertStringTemplateEquals("#set $obj.Parameters.Name = 'Eric'", "");
+        assertStringTemplateEquals("#set $obj1.Parameters.Name = 'Eric'", "");
     }
 
     public void testBinarySetGet()
     {
         assertStringTemplateEquals(
-                "#set $obj.Parameters.Name = 'Eric'\n" +
-                "$obj.Parameters.Name", "Eric");
+                "#set $obj1.Parameters.Name = 'Eric'\n" +
+                "$obj1.Parameters.Name", "Eric");
+    }
+
+    public void testBinaryMultiLevelGet()
+    {
+        assertStringTemplateEquals("$obj2.Parameters.BinaryLevelTwo.Parameters.Name", "Brian");
+    }
+
+    public void testBinaryMultiLevelSet()
+    {
+        assertStringTemplateEquals("#set $obj2.Parameters.BinaryLevelTwo.Parameters.Name = 'Keats'", "");
+    }
+
+    public void testBinaryMultiLevelSetGet()
+    {
+        assertStringTemplateEquals(
+           "#set $obj2.Parameters.BinaryLevelTwo.Parameters.Name = 'Lane'\n"+
+           "$obj2.Parameters.BinaryLevelTwo.Parameters.Name", "Lane");
     }
 
     protected void stuffContext( Context context ) throws Exception
     {
-        final Wrapper value = new Wrapper();
-        value.setParameters( "Name", "Marc");
+        final Wrapper value1 = new Wrapper();
+        value1.setParameters( "Name", "Marc");
 
-        context.put( "obj", value );
+        context.put( "obj1", value1 );
+
+        final Wrapper value2 = new Wrapper();
+        final Wrapper subWrapper = new Wrapper();
+        subWrapper.setParameters( "Name", "Brian");
+        value2.setParameters( "BinaryLevelTwo", subWrapper);
+        context.put( "obj2", value2 );
     }
 }
