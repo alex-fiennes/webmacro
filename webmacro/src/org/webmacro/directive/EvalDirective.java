5f710765bd0327543dd9c0fdf48b7ed09d9d0cfa
keats <keats>
2005-04-01 05:18:50 +0000

    Added ability to eval a String.  Eg, #eval "\$x \$y".

--- webmacro/src/org/webmacro/directive/EvalDirective.java
+++ webmacro/src/org/webmacro/directive/EvalDirective.java
@@ -29,9 +29,9 @@ package org.webmacro.directive;
 import org.webmacro.*;
 import org.webmacro.engine.BuildContext;
 import org.webmacro.engine.BuildException;
+import org.webmacro.engine.StringMacro;
+import org.webmacro.engine.StringTemplate;
 import org.webmacro.engine.Variable;
-
-import org.webmacro.directive.*;
 
 /**
  *
@@ -45,141 +45,165 @@ import org.webmacro.directive.*;
 
 public class EvalDirective extends org.webmacro.directive.Directive
 {
-    
-    private static final int EVAL_VAR = 1;
-    private static final int EVAL_USING = 2;
-    private static final int EVAL_MAP_EXPR = 3;
-    private static final int MAX_RECURSION_DEPTH = 100;
-    
-    //private Macro _evalMacro;
-    private Variable _evalTarget;
-    private Object _mapExpr = null;
-    
-    private static final ArgDescriptor[]
-    myArgs = new ArgDescriptor[]
-    {
-        new LValueArg(EVAL_VAR),
-        new OptionalGroup(2),
-        new KeywordArg(EVAL_USING, "using"),
-        new RValueArg(EVAL_MAP_EXPR)
-    };
-    
-    private static final DirectiveDescriptor
-    myDescr = new DirectiveDescriptor("eval", null, myArgs, null);
-    
-    public static DirectiveDescriptor getDescriptor()
-    {
-        return myDescr;
-    }
-    
-    /** Creates a new instance of EvalDirective */
-    public EvalDirective()
-    {
-    }
-    
-    public Object build(DirectiveBuilder builder, BuildContext bc)
-    throws BuildException
-    {
-        try
-        {
-            _evalTarget = (Variable)builder.getArg(EVAL_VAR, bc);
-            //_evalMacro = (Macro)o;
-        }
-        catch (ClassCastException e)
-        {
-            throw new NotVariableBuildException(myDescr.name, e);
-        }
-        if (builder.getArg(EVAL_USING) != null)
-        {
-            // "using" keyword specified, get map expression
-            _mapExpr = builder.getArg(EVAL_MAP_EXPR, bc);
-        }
-        //_result = (org.webmacro.engine.Block)builder.getArg(TEMPLET_RESULT, bc);
-        return this;
-    }
-    
-    public void write(org.webmacro.FastWriter out, org.webmacro.Context context) throws org.webmacro.PropertyException, java.io.IOException
-    {
-        try
-        {
-            String s = null;
-            Context c = null;
-            
-            Macro macro = (Macro)_evalTarget.getValue(context);
-            if (_mapExpr == null)
+
+   private static final int EVAL_EXPR = 1;
+   private static final int EVAL_USING = 2;
+   private static final int EVAL_MAP = 3;
+   private static final int MAX_RECURSION_DEPTH = 100;
+
+   //private Macro _evalMacro;
+   private Object _evalTarget;
+   private Object _mapExpr = null;
+
+   private static final ArgDescriptor[] myArgs =
+      new ArgDescriptor[] {
+         new RValueArg(EVAL_EXPR),
+         new OptionalGroup(2),
+         new KeywordArg(EVAL_USING, "using"),
+         new RValueArg(EVAL_MAP)};
+
+   private static final DirectiveDescriptor myDescr =
+      new DirectiveDescriptor("eval", null, myArgs, null);
+
+   public static DirectiveDescriptor getDescriptor()
+   {
+      return myDescr;
+   }
+
+   /** Creates a new instance of EvalDirective */
+   public EvalDirective()
+   {
+   }
+
+   public Object build(DirectiveBuilder builder, BuildContext bc)
+      throws BuildException
+   {
+      _evalTarget = builder.getArg(EVAL_EXPR, bc);
+      if (builder.getArg(EVAL_USING) != null)
+      {
+         // "using" keyword specified, get map expression
+         _mapExpr = builder.getArg(EVAL_MAP, bc);
+      }
+      //_result = (org.webmacro.engine.Block)builder.getArg(TEMPLET_RESULT, bc);
+      return this;
+   }
+
+   public void write(org.webmacro.FastWriter out, org.webmacro.Context context)
+      throws org.webmacro.PropertyException, java.io.IOException
+   {
+      try
+      {
+         String res = null;
+         //Context c = null;
+
+         Macro macro = null;
+
+         // The target may be:
+         // 1) a variable referencing a Macro
+         // 2) a String to be evaluated as a template
+         // 3) an expression that evaluates into one of the above?
+         Object targ = _evalTarget;
+         if (targ instanceof Variable)
+         {
+            // if variable, get it's value
+            targ = ((Variable) _evalTarget).getValue(context);
+         }
+         if (targ instanceof String)
+         {
+            // convert string to template
+            targ = new StringMacro((String) targ);
+         }
+         if (targ instanceof Macro)
+         {
+            macro = (Macro) targ;
+         }
+         else
+         {
+            // throw and exception ... we don't know what to do with this!
+            throw new PropertyException("Invalid argument to #eval directive.  First arg must be a Macro or a String.");
+         }
+         if (_mapExpr == null)
+         {
+            // no map specified, use current context
+            res = (String) macro.evaluate(context);
+         }
+         else
+         {
+            Object argmap = _mapExpr;
+            if (argmap instanceof Macro)
             {
-                // no map specified, use current context
-                s = (String)macro.evaluate(context);
+               argmap = ((Macro) argmap).evaluate(context);
             }
-            else
+            if (!(argmap instanceof java.util.Map))
             {
-                Object o = _mapExpr;
-                if (o instanceof Macro)
-                {
-                    o = ((Macro)o).evaluate(context);
-                }
-                if (!(o instanceof java.util.Map))
-                {
-                    throw new PropertyException("The supplied expression did not evaluate to a java.util.Map instance.");
-                }
-                // check for max recursion
-                int recursionDepth = 0;
-                if (context.containsKey("EvalDepth"))
-                { // check the value
-                    try {
-                        recursionDepth = ((Integer)context.get("EvalDepth")).intValue();
-                        recursionDepth++;
-                        if (recursionDepth > MAX_RECURSION_DEPTH)
-                        {
-                            throw new PropertyException(
-                            "ERROR: A recursive call to #eval exceeded the maximum depth of " 
-                            + MAX_RECURSION_DEPTH);
-                        }
-                    }
-                    catch (Exception e){
-                        // something bad happend, leave depth at default
-                    }
-                }
-                java.util.Map outerVars = null;
-                if (context.containsKey("OuterVars"))
-                { // check the value
-                    try {
-                        outerVars = (java.util.Map)context.get("OuterVars");
-                    }
-                    catch (Exception e){
-                        // something bad happend, use vars from calling context
-                    }
-                }
-                if (outerVars == null) outerVars = context.getMap();
-                c = new Context(context.getBroker());                
-                // replace _variables map with supplied map
-                c.setMap((java.util.Map)o);
-                // put current depth into the new context
-                c.put("EvalDepth", recursionDepth);
-                // add a reference to parent context variables
-                c.put("OuterVars", outerVars);     
-                // add a reference to this macro
-                c.put("Self", macro);
-                s = (String)macro.evaluate(c);
+               throw new PropertyException("The supplied expression did not evaluate to a java.util.Map instance.");
             }
-            out.write(s);
-        }
-        catch (Exception e)
-        {
-            if (e instanceof PropertyException) throw (PropertyException)e;
-            throw new PropertyException("#eval: Unable to evaluate macro.", e);
-        }
-    }
-    
-    public void accept(TemplateVisitor v)
-    {
-        v.beginDirective(myDescr.name);
-        v.visitDirectiveArg("EvalTarget", _evalTarget);
-        if (_mapExpr != null){
-            v.visitDirectiveArg("EvalKeyword", "using");
-            v.visitDirectiveArg("EvalMap", _mapExpr);
-        }        
-        v.endDirective();
-    }
-    
+            // check for max recursion
+            int recursionDepth = 0;
+            if (context.containsKey("EvalDepth"))
+            { // check the value
+               try
+               {
+                  recursionDepth =
+                     ((Integer) context.get("EvalDepth")).intValue();
+                  recursionDepth++;
+               }
+               catch (Exception e)
+               {
+                  // something bad happend, leave depth at default
+               }
+					if (recursionDepth > MAX_RECURSION_DEPTH)
+					{
+						throw new PropertyException(
+							"ERROR: A recursive call to #eval exceeded the maximum depth of "
+								+ MAX_RECURSION_DEPTH);
+					}
+            }
+            java.util.Map outerVars = null;
+            if (context.containsKey("OuterVars"))
+            { // check the value
+               try
+               {
+                  outerVars = (java.util.Map) context.get("OuterVars");
+               }
+               catch (Exception e)
+               {
+                  // something bad happened, use vars from calling context
+               }
+            }
+            if (outerVars == null)
+               outerVars = context.getMap();
+            Context c = new Context(context.getBroker());
+            // replace _variables map with supplied map
+            c.setMap((java.util.Map) argmap);
+            // put current depth into the new context
+            c.put("EvalDepth", recursionDepth);
+            // add a reference to parent context variables
+            c.put("OuterVars", outerVars);
+            // add a reference to this macro
+            c.put("Self", macro);
+            res = (String) macro.evaluate(c);
+         }
+         out.write(res);
+      }
+      catch (Exception e)
+      {
+         if (e instanceof PropertyException)
+            throw (PropertyException) e;
+         throw new PropertyException("#eval: Unable to evaluate macro.", e);
+      }
+   }
+
+   public void accept(TemplateVisitor v)
+   {
+      v.beginDirective(myDescr.name);
+      v.visitDirectiveArg("EvalTarget", _evalTarget);
+      if (_mapExpr != null)
+      {
+         v.visitDirectiveArg("EvalKeyword", "using");
+         v.visitDirectiveArg("EvalMap", _mapExpr);
+      }
+      v.endDirective();
+   }
+
 }
