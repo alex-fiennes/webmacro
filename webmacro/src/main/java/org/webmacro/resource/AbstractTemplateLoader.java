eab9a7f626421eccf66b171880f40f487f70ef5e
timp <timp>
2010-02-25 23:03:38 +0000

    Unused import

--- webmacro/src/main/java/org/webmacro/resource/AbstractTemplateLoader.java
+++ webmacro/src/main/java/org/webmacro/resource/AbstractTemplateLoader.java
@@ -26,7 +26,6 @@ import org.slf4j.LoggerFactory;
 import org.webmacro.Broker;
 import org.webmacro.InitException;
 import org.webmacro.util.Settings;
-import org.webmacro.util.WMEval;
 
 /**
  * Abstract implementation of TemplateLoader, that stores broker
